6f4913072aa6a2263007eddd92b260bcdb21e2a4
tschudid <14889202+tschudid@users.noreply.github.com>
2023-11-17 07:23:18 +0100

    simplify GuardianProof

--- src/eg/src/guardian_proof.rs
+++ src/eg/src/guardian_proof.rs
@@ -4,23 +4,44 @@ use crate::{
     election_parameters::ElectionParameters,
     guardian::GuardianIndex,
     guardian_public_key_info::GuardianPublicKeyInfo,
-    guardian_secret_key::{CoefficientCommitments, GuardianSecretKey, SecretCoefficients},
+    guardian_secret_key::GuardianSecretKey,
     hash::{eg_h, HValue},
 };
-use anyhow::{ensure, Context, Result};
+use anyhow::{ensure, Context, Ok, Result};
 use num_bigint::BigUint;
 use serde::{Deserialize, Serialize};
 use util::{csprng::Csprng, integer_util::to_be_bytes_left_pad};
 
 #[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct CoefficientProof {
+pub struct PossessionProofChallenge(
+    #[serde(
+        serialize_with = "util::biguint_serde::biguint_serialize",
+        deserialize_with = "util::biguint_serde::biguint_deserialize"
+    )]
+    pub BigUint,
+);
+
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct PossessionProofResponse(
+    #[serde(
+        serialize_with = "util::biguint_serde::biguint_serialize",
+        deserialize_with = "util::biguint_serde::biguint_deserialize"
+    )]
+    pub BigUint,
+);
+
+/// Proof of Possession for the coefficient in a guardian's secret key.
+#[derive(Clone, Debug, Serialize, Deserialize)]
+pub struct GuardianProof {
+    /// Guardian number, 1 <= i <= [`crate::varying_parameters::VaryingParameters::n`].
+    pub i: GuardianIndex,
     /// Challenge vector
-    pub c: Vec<BigUint>,
+    pub c: Vec<PossessionProofChallenge>,
     /// Response vector
-    pub v: Vec<BigUint>,
+    pub v: Vec<PossessionProofResponse>,
 }
 
-impl CoefficientProof {
+impl GuardianProof {
     /// This function computes the challenge for the coefficient NIZK as specified in Equation (12)
     /// The arguments are
     /// - h_p - the parameter base hash
@@ -28,13 +49,13 @@ impl CoefficientProof {
     /// - j - the coefficient index
     /// - capital_k_i_j - the coefficient commitment
     /// - h_i_j - the commit message
-    pub fn challenge(
+    fn challenge(
         h_p: HValue,
         i: u32,
         j: u32,
         capital_k_i_j: &BigUint,
         h_i_j: &BigUint,
-    ) -> BigUint {
+    ) -> PossessionProofChallenge {
         // v = 0x10 | b(i,4) | b(j,4) | b(capital_k_i_j,512) | b(h_i_j,j,512)
         let mut v = vec![0x10];
         v.extend_from_slice(i.to_be_bytes().as_slice());
@@ -43,7 +64,7 @@ impl CoefficientProof {
         v.extend_from_slice(to_be_bytes_left_pad(h_i_j, 512).as_slice());
         let c = eg_h(&h_p, &v);
         //The challenge is not reduced modulo q (cf. Section 5.4)
-        BigUint::from_bytes_be(c.0.as_slice())
+        PossessionProofChallenge(BigUint::from_bytes_be(c.0.as_slice()))
     }
 
     /// This function computes coefficient NIZK proof as specified in Section 3.2.2
@@ -51,21 +72,16 @@ impl CoefficientProof {
     /// - csprng - secure randomness generator
     /// - election_parameters - the election parameters
     /// - h_p - the parameter base hash
-    /// - i - the guardian index
-    /// - secret_coefficients - the coefficients
-    /// - coefficient_commitments - the coefficients commitments
+    /// - secret_key - the guardian `GuardianSecretKey`
     pub fn new(
         csprng: &mut Csprng,
         election_parameters: &ElectionParameters,
         h_p: HValue,
-        i: GuardianIndex,
-        secret_coefficients: &SecretCoefficients,
-        coefficient_commitments: &CoefficientCommitments,
+        secret_key: &GuardianSecretKey,
     ) -> Self {
         let fixed_parameters = &election_parameters.fixed_parameters;
         let varying_parameters = &election_parameters.varying_parameters;
         let k = varying_parameters.k;
-        let i = i.get_one_based_u32();
 
         // Compute commit message
         let u_vec = (0..k.get_one_based_u32())
@@ -80,52 +96,63 @@ impl CoefficientProof {
             .map(|j| {
                 Self::challenge(
                     h_p,
-                    i,
+                    secret_key.i.get_one_based_u32(),
                     j,
-                    &coefficient_commitments.0[j as usize].0,
+                    &secret_key.coefficient_commitments.0[j as usize].0,
                     &h_vec[j as usize],
                 )
             })
-            .collect::<Vec<BigUint>>();
+            .collect::<Vec<PossessionProofChallenge>>();
         // Compute response vector
         let v_vec = (0..k.get_one_based_usize())
             .map(|j| {
-                fixed_parameters
-                    .q
-                    .subtract_group_elem(&u_vec[j], &(&c_vec[j] * &secret_coefficients.0[j].0))
+                PossessionProofResponse(fixed_parameters.q.subtract_group_elem(
+                    &u_vec[j],
+                    &(&c_vec[j].0 * &secret_key.secret_coefficients.0[j].0),
+                ))
             })
-            .collect::<Vec<BigUint>>();
-        CoefficientProof { c: c_vec, v: v_vec }
+            .collect::<Vec<PossessionProofResponse>>();
+        GuardianProof {
+            i: secret_key.i,
+            c: c_vec,
+            v: v_vec,
+        }
     }
 
-    /// This function verifies a coefficient NIZK proof with respect to given commitments and parameters
+    /// This function verifies a `GuardianProof` with respect to a given `GuardianPublicKeyInfo`
     /// This is corresponds to Verification 2 "Guardian public-key validation"
     /// The arguments are
-    /// - self - the NIZK proof
+    /// - self - the `GuardianProof`
     /// - election_parameters - the election parameters
     /// - h_p - the parameter base hash
-    /// - i - the guardian index
-    /// - coefficient_commitments - the coefficients commitments
-    pub fn verify(
+    /// - public_key_info - the `GuardianPublicKeyInfo`
+    pub fn validate(
         &self,
         election_parameters: &ElectionParameters,
         h_p: HValue,
-        i: GuardianIndex,
-        coefficient_commitments: &CoefficientCommitments,
-    ) -> bool {
+        public_key_info: &dyn GuardianPublicKeyInfo,
+    ) -> Result<()> {
+        ensure!(
+            self.i == public_key_info.i(),
+            "The guardian indices of the proof and the public key must match."
+        );
+
         let fixed_parameters = &election_parameters.fixed_parameters;
         let varying_parameters = &election_parameters.varying_parameters;
         let k = varying_parameters.k;
-        let i = i.get_one_based_u32();
+
+        let i = self.i.get_one_based_u32();
+        let coefficient_commitments = public_key_info.coefficient_commitments();
+
         // Equation (2.1)
         let h_vec = (0..k.get_one_based_usize())
             .map(|j| {
                 fixed_parameters
                     .g
-                    .modpow(&self.v[j], fixed_parameters.p.borrow())
+                    .modpow(&self.v[j].0, fixed_parameters.p.borrow())
                     * coefficient_commitments.0[j]
                         .0
-                        .modpow(&self.c[j], fixed_parameters.p.borrow())
+                        .modpow(&self.c[j].0, fixed_parameters.p.borrow())
                     % fixed_parameters.p.as_ref()
             })
             .collect::<Vec<BigUint>>();
@@ -141,76 +168,13 @@ impl CoefficientProof {
                 .modpow(fixed_parameters.q.borrow(), fixed_parameters.p.borrow())
                 == one;
             // 2.B
-            verified &=
-                (zero <= self.v[j as usize]) && (self.v[j as usize] < *fixed_parameters.q.borrow());
+            verified &= (zero <= self.v[j as usize].0)
+                && (self.v[j as usize].0 < *fixed_parameters.q.borrow());
             // 2.C
-            verified &=
-                self.c[j as usize] == Self::challenge(h_p, i, j, capital_k_i_j, &h_vec[j as usize])
+            verified &= self.c[j as usize].0
+                == Self::challenge(h_p, i, j, capital_k_i_j, &h_vec[j as usize]).0
         }
-        verified
-    }
-}
-
-/// Proof of Possession for the coefficient in a guardian's secret key.
-#[derive(Clone, Debug, Serialize, Deserialize)]
-pub struct GuardianProof {
-    /// Guardian number, 1 <= i <= [`crate::varying_parameters::VaryingParameters::n`].
-    pub i: GuardianIndex,
-
-    /// NiZK for polynomial coefficients
-    pub coefficient_proof: CoefficientProof,
-}
-
-impl GuardianProof {
-    /// Generates a `GuardianProof` given the election parameter and a secret key
-    pub fn generate(
-        csprng: &mut Csprng,
-        election_parameters: &ElectionParameters,
-        h_p: HValue,
-        secret_key: &GuardianSecretKey,
-    ) -> Self {
-        let i = secret_key.i;
-        let coefficient_proof = CoefficientProof::new(
-            csprng,
-            election_parameters,
-            h_p,
-            i,
-            &secret_key.secret_coefficients,
-            &secret_key.coefficient_commitments,
-        );
-        assert!(coefficient_proof.verify(
-            election_parameters,
-            h_p,
-            i,
-            &secret_key.coefficient_commitments
-        ));
-        GuardianProof {
-            i,
-            coefficient_proof,
-        }
-    }
-
-    /// Validates a `GuardianProof` given the election parameter and a `GuardianPublicKeyInfo`
-    pub fn validate(
-        &self,
-        election_parameters: &ElectionParameters,
-        h_p: HValue,
-        public_key_info: &dyn GuardianPublicKeyInfo,
-    ) -> Result<()> {
-        ensure!(
-            self.i == public_key_info.i(),
-            "The guardian indices of the proof and the public key must match."
-        );
-
-        ensure!(
-            self.coefficient_proof.verify(
-                election_parameters,
-                h_p,
-                self.i,
-                public_key_info.coefficient_commitments()
-            ),
-            "The proof is invalid."
-        );
+        ensure!(verified, "The proof is invalid.");
 
         Ok(())
     }
@@ -268,7 +232,7 @@ mod test {
         let guardian_proofs = guardian_secret_keys
             .iter()
             .map(|secret_key| {
-                GuardianProof::generate(&mut csprng, &election_parameters, hashes.h_p, secret_key)
+                GuardianProof::new(&mut csprng, &election_parameters, hashes.h_p, secret_key)
             })
             .collect::<Vec<_>>();
 
@@ -300,7 +264,7 @@ mod test {
         let sk_two =
             GuardianSecretKey::generate(&mut csprng, &election_parameters, index_two, None);
 
-        let proof = GuardianProof::generate(&mut csprng, &election_parameters, hashes.h_p, &sk_one);
+        let proof = GuardianProof::new(&mut csprng, &election_parameters, hashes.h_p, &sk_one);
 
         assert!(
             proof
